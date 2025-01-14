c3a00dfcbb24bd9eed6f717430e39206e7227898
anouar el barry <elbarryanwar37@gmail.com>
2025-01-14 20:34:17 +0100

    add getters and setters

--- modal/user.php
+++ modal/user.php
@@ -1,20 +1,60 @@
 <?php 
 include_once __DIR__ ."/../Database/Database.php";
 class User {
+    const ROLE_ADMIN = "admin";
+    const ROLE_TEACHER = "teacher";
+    const ROLE_STUDENT = "student";
+    const ACTIVE = "ACTIVE";
+    const ARCHIVED = "ARCHIVED";
+    const PENDING = "PENDING";
+    protected $status;
+    protected $id;
+    protected $username;
+    protected $email;
+    protected $role;
     private Database $db;
 
-    public function __construct(){
+    public function __construct() {
         $this->db = new Database();
     }
-    public function regester($username,$email,$password) {
-        
-    }
-    public function login($email,$password) {}
 
-    public function setRole($userId, $role) {
-        // To be implemented
+    // Setters
+    public function setId($id) {
+        $this->id = $id;
     }
-    public function getUserById($userId) {
-        // To be implemented
+
+    public function setUsername($username) {
+        $this->username = $username;
     }
+    public function setStatus($status) {
+         $this->status = $status;
+    }
+    public function setEmail($email) {
+        $this->email = $email;
+    }
+
+    public function setRole($role) {
+        $this->role = $role;
+    }
+
+    // Getters
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getUsername() {
+        return $this->username;
+    }
+
+    public function getEmail() {
+        return $this->email;
+    }
+
+    public function getRole() {
+        return $this->role;
+    }
+    public function getStatus() {
+        return $this->status;
+    }
+
 }
