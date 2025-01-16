5eea73fd13d328e08d7110d17ead9cc593ae7c20
anouar el barry <elbarryanwar37@gmail.com>
2025-01-16 13:05:43 +0100

    creat class requests controller

---
+++ controller/admin/requestsController.php
@@ -0,0 +1,13 @@
+<?php
+class RequestsController{
+    private Admin $admin;
+    function __construct(){
+        $this->admin = new Admin();
+}
+   public function accept(){
+     
+   }
+   public function reject(){
+    
+   }
+}
