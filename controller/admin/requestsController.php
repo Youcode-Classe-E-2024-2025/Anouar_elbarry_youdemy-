a7f47a577981d1b3c126dc58bf6a2d7c17775262
anouar el barry <elbarryanwar37@gmail.com>
2025-01-16 13:06:14 +0100

    creat accept methode

--- controller/admin/requestsController.php
+++ controller/admin/requestsController.php
@@ -5,7 +5,16 @@ class RequestsController{
         $this->admin = new Admin();
 }
    public function accept(){
-     
+       if(isset($_GET["id"])){
+        $id = $_GET["id"];
+        $result = $this->admin->validateTeacher($id);
+        if($result){
+            $_SESSION['successRE'] = 'the teacher accepted succesfuly';
+        }else{
+            $_SESSION['errorRE'] = "no teacher with this id";
+        }
+        header("location: ../../view/admin/requests.php");
+       }
    }
    public function reject(){
     
