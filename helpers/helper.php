6a91c484dd660be4d444531b9fbbf0d05e04a2d7
anouar el barry <elbarryanwar37@gmail.com>
2025-01-17 09:12:57 +0100

    creat function to change date format

--- helpers/helper.php
+++ helpers/helper.php
@@ -4,3 +4,7 @@ function dd($var){
    print_r($var);
    die();
 }
+function convertDateFormat($dateString) {
+    $date = DateTime::createFromFormat('Y-m-d H:i:s', $dateString);
+    return $date->format('F d Y H:i:s');
+}
